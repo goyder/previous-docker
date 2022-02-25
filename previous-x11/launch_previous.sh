6753295c2906e9d0d8d1d693d1261ce374c7c38e
Ubuntu <ubuntu@ip-172-31-36-224.ap-southeast-2.compute.internal>
2022-02-25 07:52:11 +0000

    Reorganise X11 model

--- previous-x11/launch_previous.sh
+++ previous-x11/launch_previous.sh
@@ -2,6 +2,8 @@ docker run \
 	--rm \
 	-it \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-v $HOME/.Xauthority:/root/.Xauthority \
+	--privileged \
 	-e DISPLAY=unix$DISPLAY \
 	--name previous \
 	previous
